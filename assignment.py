a8ec233697f91e172df136c85a63c3683d1bd017
friedwook <yoojw1118@gmail.com>
2025-04-11 11:54:21 +0900

    4/11

--- assignment.py
+++ assignment.py
@@ -8,9 +8,11 @@ if judjgment_number == 1: #입력값이 1인 경우
     answer =  eval(expression) #수식 계산
     print(expression, '결과는' ,answer, '입니다') #결과 출력
 elif judjgment_number == 2: #입력값이 2인 경우
-    #첫번째, 두번째 숫자 받기기
+    #첫번째, 두번째 숫자 받기
     firstNumber = int(input('***첫 번째 숫자를 입력하세요:')) 
     secondNumber = int(input('***두 번째 숫자를 입력하세요:'))
-    count_between_numbers = abs(secondNumber-firstNumber) + 1 #두 수 사이 수의 개수 구하기
+    count_between_numbers = abs(secondNumber-firstNumber) + 1 #두 수 사이 수의 개수 구구하기
     answer = count_between_numbers*(firstNumber+secondNumber)/2 #두 수 사이 숫자의 개수 * 평균 = 답
     print(firstNumber,"+...+",secondNumber, '는', answer,'입니다') #결과 출력
+
+\
